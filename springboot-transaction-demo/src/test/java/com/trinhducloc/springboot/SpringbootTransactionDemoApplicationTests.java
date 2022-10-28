c72abddf908b3d3a51dd84a023e743df5dc6eb13
Loc Trinh Duc (VN) <Loc.TrinhD@homecredit.vn>
2022-10-28 15:50:43 +0700

    Initial commit

---
+++ springboot-transaction-demo/src/test/java/com/trinhducloc/springboot/SpringbootTransactionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinhducloc.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTransactionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
