a28333a87550e21b4f817caba68bddff79dcdfc7
TrinhDucLoc <westudymore@gmail.com>
2022-10-31 13:41:23 +0700

    Update Transaction

--- springboot-transaction-demo/src/main/java/com/trinhducloc/springboot/SpringbootTransactionDemoApplication.java
+++ springboot-transaction-demo/src/main/java/com/trinhducloc/springboot/SpringbootTransactionDemoApplication.java
@@ -2,8 +2,10 @@ package com.trinhducloc.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringbootTransactionDemoApplication {
 
 	public static void main(String[] args) {
--- springboot-transaction-demo/src/main/java/com/trinhducloc/springboot/controller/OrderController.java
+++ springboot-transaction-demo/src/main/java/com/trinhducloc/springboot/controller/OrderController.java
@@ -2,24 +2,27 @@ package com.trinhducloc.springboot.controller;
 
 import com.trinhducloc.springboot.dto.OrderRequest;
 import com.trinhducloc.springboot.dto.OrderResponse;
+import com.trinhducloc.springboot.entity.Order;
 import com.trinhducloc.springboot.service.OrderService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/orders")
+@RequestMapping(value = "/api/v1/orders", method = RequestMethod.GET)
 public class OrderController {
     private OrderService orderService;
-
+//     TO_DO: private PaymentService paymentService;
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
     @PostMapping
     public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest){
+        // create order -> success
+//        Order order = orderService.saveOrder();
+        //save payment: paymentServiceSavePayment(order)
+        // create payment -> ex
+
         return ResponseEntity.ok(orderService.placeOrder(orderRequest));
     }
 }
